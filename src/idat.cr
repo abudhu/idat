2727a43e1007aaa1cf2ac754c38df1fce543c7e5
Amit Budhu <amitbudhu@LinuxDesktop.lan>
2022-01-02 09:37:00 -0800

    Solutions + Quick Build Test for OS Detection

--- src/idat.cr
+++ src/idat.cr
@@ -2,5 +2,16 @@
 module Idat
   VERSION = "0.1.0"
 
-  toml = TOML.parse(File.read("/PATH/TO/FILE"))
+  #toml = TOML.parse(File.read("/PATH/TO/FILE"))
+
+  {% if flag?(:linux) %}
+    puts "You are on Linux"
+  {% elsif flag?(:darwin) %}
+    puts "You are on Mac"
+  {% elsif flag?(:win32) %}
+    puts "You are on Windows"
+  {% else %}
+    puts "You are unsupported"
+  {% end %}
+
 end
