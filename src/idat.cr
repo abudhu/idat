88c01439d7440d26069d63b225ec255fd30c32da
Amit Budhu <amitbudhu@LinuxDesktop.lan>
2022-01-02 15:26:01 -0800

    Pushing current work

--- src/idat.cr
+++ src/idat.cr
@@ -1,17 +1,56 @@
 # TODO: Write documentation for `Idat`
+require "toml"
+require "colorize"
+
+def validateToml(toml_file toml)
+  toml = toml.as(Hash)
+  toml.has_key?("projectInfo")
+  toml.has_key?("projectSettings")
+end
+
+def getSystem()
+  {% if flag?(:linux) %}
+    os = "Linux"
+  {% elsif flag?(:darwin) %}
+    os = "Mac"
+  {% elsif flag?(:win32) %}
+    os = "Windows"
+  {% else %}
+    os = "Unsupported"
+  {% end %}
+end
+
+def validateSystem(*, tomlSystem)
+  systemOS = getSystem
+  if tomlSystem.includes?(systemOS)
+   puts "Validating System...#{"Success".colorize(:green)}"
+  end
+end
+
 module Idat
   VERSION = "0.1.0"
 
-  #toml = TOML.parse(File.read("/PATH/TO/FILE"))
 
-  {% if flag?(:linux) %}
-    puts "You are on Linux"
-  {% elsif flag?(:darwin) %}
-    puts "You are on Mac"
-  {% elsif flag?(:win32) %}
-    puts "You are on Windows"
-  {% else %}
-    puts "You are unsupported"
-  {% end %}
+  toml = TOML.parse(File.read("solutions/ruby_install.toml"))
 
-end
+  validateToml toml_file: toml
+  
+  #puts toml.keys
+
+  startTime = Time.local
+
+  projectInfo = toml["projectInfo"].as(Hash)
+  projectSettings = toml["projectSettings"].as(Hash)
+
+  puts "Starting #{(projectInfo["name"]).colorize(:green)} at #{startTime}"
+
+  validateSystem(tomlSystem: projectSettings["system"].as(Array))
+
+  projectSteps = toml["projectSteps"].as(Hash)
+
+  projectSteps.each do | k, v |
+    puts "Running step #{k.colorize.fore(:yellow).mode(:bold)}"
+    puts "Executing #{v.as(Hash)["run"].colorize(:cyan)} command"
+  end
+
+  end
