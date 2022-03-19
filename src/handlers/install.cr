2655ceaa3eec05f8ded5f8470a6c8fc854e8557e
Amit Budhu <abudhu@gmail.com>
2022-03-18 17:27:20 -0700

    Beginning simple install

--- src/handlers/install.cr
+++ src/handlers/install.cr
@@ -10,7 +10,31 @@ module Idat
       # Or if the single link got subsituted and contains and array
       #puts params.is_a?(Array)
       #puts params.is_a?(String)
-      puts "Installing... #{params.colorize(:green)}"
+      @params = params.as(String)
+      @sysInstaller = getLinuxDistro()
+      installApp()
     end
+
+    def installApp()
+      puts "Installing... #{@params.colorize(:green)}"
+      puts @sysInstaller
+      #Process.run(@sysInstaller, args=@params, shell: true)
+    end
+
+    private def getLinuxDistro()
+      io = IO::Memory.new
+      Process.run("grep '^ID_LIKE' /etc/os-release", shell: true, output: io)
+      linuxDistro = io.to_s.lchop("ID_LIKE=").chomp
+      
+      puts linuxDistro
+
+      case linuxDistro
+      when "debian"
+        @sysInstaller = "apt"
+      else
+        @sysInstaller = "unknown"
+      end
+    end
+
   end
 end
