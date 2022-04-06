49d7453c110b17c64b77ddd6538ca733ea11f087
Amit Budhu <abudhu@gmail.com>
2022-04-06 10:42:07 -0700

    Install works with simple name, array of items, or a subsituted variable

--- src/handlers/install.cr
+++ src/handlers/install.cr
@@ -3,32 +3,44 @@ require "./common.cr"
 
 module Idat
   class InstallFunctions
+    @params : Array(TOML::Type) | String
     def initialize(params, projectVariables)
       @projectVariables = Hash(String, TOML::Type).new
       # This needs to figure out if you use Apt, Powershell, Choco, 
       # Then needs to figure out if you gave it an array
       # Or if its a single app
       # Or if the single link got subsituted and contains and array
-      #puts params.is_a?(Array)
-      #puts params.is_a?(String)
-      @params = params.as(String)
+      
+      if params.is_a?(Array)
+        @params = params.as(Array)
+      else
+        @params = params.as(String)
+      end
       @projectVariables = projectVariables
       @sysInstaller = getLinuxDistro()
     end
 
     def installApp()
-
       cf = CommonFunctions.new()
-      newCmd = cf.substituteVariables(@params, @projectVariables)
-      if newCmd.is_a?(Array)
-        newCmd.each do | cmd |
-          puts "Installing... #{cmd.colorize(:green)}"
-          cf.processRun("#{@sysInstaller} #{cmd} -y")
+      
+      if @params.is_a?(Array)
+        @params.as(Array).each do | app |
+          puts "Installing... #{app.colorize(:green)}"
+          cf.processRun("#{@sysInstaller} #{app} -y")
         end
       else
-        puts "Installing... #{@params.colorize(:green)}"
-        cf.processRun("#{@sysInstaller} #{@params} -y")
+        newCmd = cf.substituteVariables(@params.as(String), @projectVariables)
+        if newCmd.is_a?(Array)
+          newCmd.each do | cmd |
+            puts "Installing... #{cmd.colorize(:green)}"
+            cf.processRun("#{@sysInstaller} #{cmd} -y")
+          end
+        else
+          puts "Installing... #{@params.colorize(:green)}"
+          cf.processRun("#{@sysInstaller} #{@params} -y")
+        end
       end
+      
 
     end
 
